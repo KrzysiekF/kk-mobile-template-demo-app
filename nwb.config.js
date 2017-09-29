e2540dd9413ac8fb09fdf9c56f2c512c1e4c2332
kfurtak <kfurtak@najlepszefoto.pl>
2017-09-29 08:31:08 +0200

    - Theme change

--- nwb.config.js
+++ nwb.config.js
@@ -1,15 +1,3 @@
 module.exports = {
-  type: 'web-app',
-
-  webpack: {
-    extractText: {
-      filename: '[name].css'
-    },
-    extra: {
-      output: {
-        filename: '[name].js',
-        chunkFilename: '[name].js'
-      }
-    }
-  }
+  type: 'web-app'
 }
--- src/index.js
+++ src/index.js
@@ -2,114 +2,139 @@ import { map } from 'lodash';
 import './style.css';
 
 class ThemesDemoApp {
-    constructor() {
-        this.selectedDevice = null;
+  constructor() {
+    this.selectedDevice = null;
 
-        this.options = {
-            url: window.KW_DEMO_CONFIG.themeUrl || '',
-            container: window.KW_DEMO_CONFIG.container || '#kk-demo-app',
-            qrcode: window.KW_DEMO_CONFIG.qrcode || '',
-            devices: {
-                mobile_small: {
-                    name: 'Mobile Small',
-                    size: [320, 480],
-                    menuClass: 'kk-mobile'
-                },
-                mobile_small_landscape: {
-                    name: 'Mobile Small Landscape',
-                    size: [480, 320],
-                    menuClass: 'kk-mobile-landscape'
-                },
-                mobile: {
-                    name: 'Mobile',
-                    size: [375, 667],
-                    menuClass: 'kk-mobile'
-                },
-                mobile_landscape: {
-                    name: 'Mobile Landscape',
-                    size: [667, 375],
-                    menuClass: 'kk-mobile-landscape'
-                },
-                tablet: {
-                    name: 'Tablet',
-                    size: [768, 1024],
-                    menuClass: 'kk-tablet'
-                },
-                tablet_landscape: {
-                    name: 'Tablet Landscape',
-                    size: [1024, 768],
-                    menuClass: 'kk-tablet-landscape'
-                }
+    this.options = {
+      url: window.KW_DEMO_CONFIG.themeUrl || '',
+      container: window.KW_DEMO_CONFIG.container || '#kk-demo-app',
+      qrcode: window.KW_DEMO_CONFIG.qrcode || '',
+      devices: {
+        mobile_small: {
+          name: 'Mobile Small',
+          size: [ 320, 480 ],
+          menuClass: 'kk-mobile',
+        },
+        mobile_small_landscape: {
+          name: 'Mobile Small Landscape',
+          size: [ 480, 320 ],
+          menuClass: 'kk-mobile-landscape',
+        },
+        mobile: {
+          name: 'Mobile',
+          size: [ 375, 667 ],
+          menuClass: 'kk-mobile',
+        },
+        mobile_landscape: {
+          name: 'Mobile Landscape',
+          size: [ 667, 375 ],
+          menuClass: 'kk-mobile-landscape',
+        },
+        tablet: {
+          name: 'Tablet',
+          size: [ 768, 1024 ],
+          menuClass: 'kk-tablet',
+        },
+        tablet_landscape: {
+          name: 'Tablet Landscape',
+          size: [ 1024, 768 ],
+          menuClass: 'kk-tablet-landscape',
+        },
+      },
+      themes: window.KW_DEMO_CONFIG.themes || [],
+    };
+
+    this.selectDevice('mobile_small');
+  }
+
+  bindEvents() {
+    const menuLink = document.querySelectorAll(this.options.container + ' a');
+
+    for (var i = 0; i < menuLink.length; i++) {
+      menuLink[ i ].addEventListener('click', (event) => {
+        const button = event.target;
+        this.onMenuClick(button);
+
+        event.preventDefault();
+      });
+    }
+
+    if (this.options.themes && !!this.options.themes.length) {
+      document.getElementById('kk-demo-theme-change').
+          addEventListener('change', (event) => {
+            const slug = event.target.value;
+            const frame = document.getElementById('kk-demo-frame');
+            const theme = frame.contentWindow.document.getElementById(
+                'theme-style');
+
+            if (!slug) {
+              theme.href = null;
+              return false;
             }
-        };
 
-        this.selectDevice('mobile_small');
+            theme.href = `assets/themes/${event.target.value}/style.css`;
+          });
+    }
+  }
+
+  onMenuClick(button) {
+    const { device } = button.dataset;
+
+    this.selectedDevice = device;
+
+    this.changeSize();
+  }
+
+  selectDevice(device) {
+    this.selectedDevice = device;
+    this.refreshView();
+    this.bindEvents();
+  }
+
+  changeSize() {
+    const frame = document.querySelector(this.options.container + ' iframe');
+
+    frame.style.width = this.options.devices[ this.selectedDevice ].size[ 0 ] +
+        'px';
+    frame.style.height = this.options.devices[ this.selectedDevice ].size[ 1 ] +
+        'px';
+
+    this.selectMenu();
+  }
+
+  refreshView() {
+    console.log('--> this.options.container: ', this.options.container,
+        document.querySelector(this.options.container));
+    document.querySelector(
+        this.options.container).innerHTML = this.insertTemplate();
+
+    this.selectMenu();
+  }
+
+  selectMenu() {
+    const menuLinks = document.querySelectorAll(
+        this.options.container + ' .kk-menu a');
+
+    map(menuLinks, (link) => {
+      link.classList.remove('active');
+    });
+
+    const selected = document.querySelector(
+        this.options.container + ' .kk-menu a[data-device="' +
+        this.selectedDevice + '"]');
+
+    selected.classList.add('active');
+  }
+
+  insertTemplate() {
+    console.log('--> this.selectedDevice: ', this.selectedDevice);
+    console.log('--> options: ', this.options.devices[ this.selectedDevice ]);
+
+    if (!this.selectedDevice) {
+      return (`<div>Loading...</div>`);
     }
 
-    bindEvents() {
-        const menuLink = document.querySelectorAll(this.options.container + ' a');
-
-        for (var i = 0; i < menuLink.length; i++) {
-            menuLink[i].addEventListener('click', (event) => {
-                const button = event.target;
-                this.onMenuClick(button);
-
-                event.preventDefault();
-            });
-        }
-    }
-
-    onMenuClick(button) {
-        const { device } = button.dataset;
-
-        this.selectedDevice = device;
-
-        this.changeSize();
-    }
-
-    selectDevice(device) {
-        this.selectedDevice = device;
-        this.refreshView(); 
-        this.bindEvents();
-    }
-
-    changeSize() {
-        const frame = document.querySelector(this.options.container + ' iframe');
-
-        frame.style.width = this.options.devices[this.selectedDevice].size[0] + 'px';
-        frame.style.height = this.options.devices[this.selectedDevice].size[1] + 'px';
-
-      this.selectMenu();
-    }
-
-    refreshView() {
-        console.log('--> this.options.container: ', this.options.container, document.querySelector(this.options.container));
-        document.querySelector(this.options.container).innerHTML = this.insertTemplate();
-
-        this.selectMenu();
-    }
-
-    selectMenu() {
-        const menuLinks = document.querySelectorAll(this.options.container + ' .kk-menu a');
-
-        map(menuLinks, (link) => {
-            link.classList.remove('active');
-        });
-
-        const selected = document.querySelector(this.options.container + ' .kk-menu a[data-device="'+ this.selectedDevice +'"]');
-
-        selected.classList.add('active');
-    }
-
-    insertTemplate() {
-        console.log('--> this.selectedDevice: ', this.selectedDevice);
-        console.log('--> options: ', this.options.devices[this.selectedDevice]);
-
-        if (!this.selectedDevice) {
-            return (`<div>Loading...</div>`);
-        }
-
-        return (`
+    return (`
             <div class="kk-menu">
                 ${this.insertMenu()}
             </div>
@@ -117,42 +142,57 @@ class ThemesDemoApp {
                 ${this.insertIframe()}     
             </div>
         `);
+  }
+
+  insertThemeChoose() {
+    if (!this.options.themes || !this.options.themes.length) {
+      return ('');
     }
 
-    insertMenu() {
-        const links = map(this.options.devices, (device, key) => {
-                return (`<li><a href="#" data-device="${key}" class="${device.menuClass}">${device.name}</a></li>`);
-            }).join('');
+    const labelText = `<span>Select theme:</span>`;
+    const options = map(this.options.themes, (theme) => {
+      return (`<option value="${theme.slug}">${theme.name}</option>`);
+    }).join('');
 
-        return (`
+    return (`${labelText} <select id="kk-demo-theme-change">${options}</select>`);
+  }
+
+  insertMenu() {
+    const links = map(this.options.devices, (device, key) => {
+      return (`<li><a href="#" data-device="${key}" class="${device.menuClass}">${device.name}</a></li>`);
+    }).join('');
+
+    return (`
             <div class="kk-menu">
                 <span>Select device: </span>
                 <ul>${links}</ul>
+                ${this.insertThemeChoose()}
             </div>
         `);
-    }
+  }
 
-    insertQRcode() {
-        return(`
+  insertQRcode() {
+    return (`
             <div class="kk-qrcode">
                 <div>Check on your phone!</div>
                 <img src="${this.options.qrcode}">
             </div>
         `);
-    }
+  }
 
-    insertIframe() {
-        return (`
+  insertIframe() {
+    return (`
             <div class="kk-device">
                 ${this.options.qrcode ? this.insertQRcode() : ''}
-                <iframe 
+                <iframe
+                    id="kk-demo-frame"
                     src="${this.options.url}"
                     frameborder="0"
-                    style="width: ${this.options.devices[this.selectedDevice].size[0]}px; height: ${this.options.devices[this.selectedDevice].size[1]}px;"
+                    style="width: ${this.options.devices[ this.selectedDevice ].size[ 0 ]}px; height: ${this.options.devices[ this.selectedDevice ].size[ 1 ]}px;"
                 ></iframe>
             </div>
         `);
-    }
+  }
 }
 
 new ThemesDemoApp();
